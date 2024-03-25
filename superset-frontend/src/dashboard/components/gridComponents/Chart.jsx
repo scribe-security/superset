b42e78b1970434765fe1f438a07151708e1d6c7c
timursarvarov <53015375+timursarvarov@users.noreply.github.com>
2024-03-25 13:17:05 +0200

    sh-4147

--- superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
+++ superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
@@ -390,7 +390,6 @@ class Chart extends Component {
       dashboardId: this.props.dashboardId,
       componentId: this.props.componentId,
       chartId: this.props.chart.id,
-      chartDescription: this.props.slice.description || '',
       chartName: this.props.slice.slice_name,
       chartTitle: this.props.sliceName,
       clickX: event.nativeEvent.clientX,
