a21da723700ef76d93595169ba492367102c51b9
timur <timur@scribesecurity.com>
2024-07-17 17:35:08 +0300

    Use optional chaining for event name in Chart component

--- superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
+++ superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
@@ -437,7 +437,7 @@ class Chart extends React.Component {
         this.props?.slice?.description || '',
       ),
       params: JSON.parse(JSON.stringify(rest)),
-      barName: event.name,
+      barName: event?.name,
     };
     SingletonSwitchboard.emit('DashboardChartClicked', msg);
   }
