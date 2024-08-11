4f22c3b94ca9f89fce6446896409ebe3ba8cbff6
timur <timur@scribesecurity.com>
2024-08-11 16:43:12 +0300

    Enable conditional filter application with global flag

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/index.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/index.tsx
@@ -244,6 +244,8 @@ const FilterBar: FC<FiltersBarProps> = ({
         dispatch(updateDataMask(filterId, dataMaskSelected[filterId]));
       }
     });
+    // eslint-disable-next-line no-underscore-dangle
+    window.__canApplyAllFilters__ = true;
   }, [dataMaskSelected, dispatch]);
 
   const handleClearAll = useCallback(() => {
